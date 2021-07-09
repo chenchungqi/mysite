a3787766e8578902643c77f6dc9882e6444234b0
chenchuangqi <413806370@qq.com>
2021-07-10 00:58:53 +0800

    3333

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'mysite_db',
-    #     'USER': 'ccq',
-    #     'PASSWORD': 'test123456',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'ccq',
+        'PASSWORD': 'test123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
 
 
